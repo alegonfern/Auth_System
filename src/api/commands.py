e79e523d03d836cb521cdc2510a20d2d096e0122
Alexis Gonzalez <94706121+alegonfern@users.noreply.github.com>
2023-10-16 02:05:02 +0000

    act3

--- src/api/commands.py
+++ src/api/commands.py
@@ -16,7 +16,7 @@ def setup_commands(app):
     """
     @app.cli.command("insert-test-users") # name of our command
     @click.argument("count") # argument of out command
-    def insert_test_users(count):
+    def insert_test_data(count):
         print("Creating test users")
         for x in range(1, int(count) + 1):
             user = User()
@@ -28,7 +28,3 @@ def setup_commands(app):
             print("User: ", user.email, " created.")
 
         print("All test users created")
-
-    @app.cli.command("insert-test-data")
-    def insert_test_data():
-        pass
